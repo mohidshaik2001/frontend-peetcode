2b12852d4d51df8dd609081ef41e442008dbd799
Shaik Khaja Mohid <mohidshaik2001@gmail.com>
2025-08-03 17:23:32 +0530

    Apply dist

--- src/conf/conf.js
+++
@@ -1,5 +0,0 @@
-const conf = {
-  app_url: String(import.meta.env.VITE_API_URL),
-};
-
-export default conf;
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authslice.js";
-const store = configureStore({
-  reducer: { auth: authReducer },
-});
-
-export default store;
